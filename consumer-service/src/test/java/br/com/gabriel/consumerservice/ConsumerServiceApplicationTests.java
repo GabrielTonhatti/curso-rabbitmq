8ca8e3a9d18c53c8961b74b715e6843313facfa3
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2023-11-27 19:38:39 -0300

    Iniciando consumer de Strings

---
+++ consumer-service/src/test/java/br/com/gabriel/consumerservice/ConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.consumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
